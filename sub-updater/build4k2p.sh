9081ba74e3202e0d8203ac9827cc9b73e2bef53a
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-12-14 23:15:27 +0800

    add more and release v0.1.1

--- sub-updater/build4k2p.sh
+++ sub-updater/build4k2p.sh
@@ -2,3 +2,4 @@
 CGO_ENABLED=0 GOARCH=mipsle GOOS=linux GOMIPS=softfloat go build -trimpath -ldflags '-s -w' -o clashsub.ori ./cmd/main.go
 strip ./clashsub.ori
 upx ./clashsub.ori -o ./clashsub
+rm ./clashsub.ori
