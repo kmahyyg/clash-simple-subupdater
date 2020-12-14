eaae6fb50f0be24dca164791beb33e5e16417f2d
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-12-14 23:06:04 +0800

    add build script for k2p

---
+++ sub-updater/build4k2p.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CGO_ENABLED=0 GOARCH=mipsle GOOS=linux GOMIPS=softfloat go build -trimpath -ldflags '-s -w' -o clashsub.ori ./cmd/main.go
+strip ./clashsub.ori
+upx ./clashsub.ori -o ./clashsub
